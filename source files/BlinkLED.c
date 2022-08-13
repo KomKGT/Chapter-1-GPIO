cfc73ae02e608e717b546143bf1a157e88766bbf
KomKGT <81642936+KomKGT@users.noreply.github.com>
2022-08-13 15:29:09 +0700

    Update Comment
    เพื่อความเข้าใจ

--- source files/BlinkLED.c
+++ source files/BlinkLED.c
@@ -20,11 +20,11 @@
 //---------------------------------------------------------------------------------------------
 void Init_LED(void)
 {
-	P21DO=1;	// Output = 0
+	P21DO=1;	// Output = 1 LED is on
 	P21IE=0;P21OE=1;P21OD=0;P21PU=0;	// Disable input, Enable output, CMOS output, No pull
 	P21MD3=0;P21MD2=0;P21MD1=0;P21MD0=0;	// Primary function
 
-	P20DO=1;	// Output = 1
+	P20DO=1;	// Output = 1  LED is on
 	P20IE=0;P20OE=1;P20OD=0;P20PU=0;	// Disable input, Enable output, CMOS output, No pull
 	P20MD3=0;P20MD2=0;P20MD1=0;P20MD0=0;	// Primary function
 }
@@ -34,12 +34,12 @@ void Init_LED(void)
 
 void LEDon(void)
 {
-	LED_Out=0;
+	LED_Out=0; // P21DO = 0
 	P20DO=0;
 }
 void LEDoff(void)
 {
-	LED_Out=1;
+	LED_Out=1;  // P21DO = 1
 	P20DO=1;
 }
 
