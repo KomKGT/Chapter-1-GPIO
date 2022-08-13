a307499f024321ccfc6eed04e55eb5c47f8f957d
KomKGT <81642936+KomKGT@users.noreply.github.com>
2022-08-13 12:17:42 +0700

    Update BlinkLED.c

--- source files/BlinkLED.c
+++ source files/BlinkLED.c
@@ -24,7 +24,7 @@ void Init_LED(void)
 	P21IE=0;P21OE=1;P21OD=0;P21PU=0;	// Disable input, Enable output, CMOS output, No pull
 	P21MD3=0;P21MD2=0;P21MD1=0;P21MD0=0;	// Primary function
 
-	P20DO=1;	// Output = 0
+	P20DO=1;	// Output = 1
 	P20IE=0;P20OE=1;P20OD=0;P20PU=0;	// Disable input, Enable output, CMOS output, No pull
 	P20MD3=0;P20MD2=0;P20MD1=0;P20MD0=0;	// Primary function
 }
